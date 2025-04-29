56f5c86e4f1d5e2a6cf9b5c92b6a894c8daaa033
Gopal Saini <gopal.saini.work@gmail.com>
2025-04-29 16:59:34 -0400

    Tryout some of the React 19 Latest Features.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,15 @@ export const HomePage: React.FC = () => {
   return (
     <>
       <h2>Home Page!!</h2>
+
+      {/* Meta tags for SEO */}
+      <title>Homepage</title>
+      <meta name="author" content="Gopal" />
+      <meta name="description" content="This is the homepage of my website." />
+      <link rel="author" href="https://github.com/gsaini" />
+      <meta name="keywords" content="AL/ML Engineer, Senior Software Engineer" />
+
+      <script async src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.174.0/three.tsl.js" integrity="sha512-kLVG+9hRPzp61ceEI5/hoRsQmEV9cFf4r4DOCYIBPteqgaTB6awXLAZlWUO5Dvlisok4doFjnxs75mngpXI35w==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
     </>
   );
 };
